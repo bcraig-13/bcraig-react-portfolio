7bd00bf54c7218543b2aadfc67f1526599f66be6
Brandon Craig <twilightraven13@yahoo.com>
2021-04-30 02:16:29 -0700

    Original files are copied over and everything works. Added rel='noreferrer' to some of the links to remove react warnings.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -13,7 +13,7 @@ function About() {
       </div>
       <div className="row">
         <div className="col-sm-12 col-md-12">
-          <img src={me} alt="Brandon Craig Profile Picture" className="img-fluid float-left mr-5 mb-2" title="Ugh...I need a better picture."></img>
+          <img src={me} alt="Brandon Craig" className="img-fluid float-left mr-5 mb-2" title="Ugh...I need a better picture."></img>
           <p>My name is Brandon Craig and I've just started my career as a web developer. Web design has always interested me and it's something I really should have learned sooner. It's been a ton of fun learning the different ways a webpage can be displayed and all the science/magic that goes into making it look interesting and responsive. I'm a hard and dedicated worker that loves to code and learn new things. </p>
         </div>
       </div>
@@ -23,7 +23,7 @@ function About() {
           <p>In my free time I love to play games, listen to music, watch movies, and read about science and physics. I also have a passion for storytelling, character and world-building, and even designing and creating my own games. Mostly table-top stuff, but it still counts!</p>
           <p>
             Click{" "}
-            <a href="https://drive.google.com/file/d/1difBDVvDQHclO7V9Tz_St6KBEG83npG2/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/file/d/1difBDVvDQHclO7V9Tz_St6KBEG83npG2/view?usp=sharing" rel="noreferrer" target="_blank">
               here
             </a>{" "}
             to view my resume. And check out the projects I've worked on by clicking "My Portfolio" on the navbar.
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -14,9 +14,9 @@ function Contact() {
                 <div className="col-lg-6">
                     <p>Want to get in touch? Feel free to contact me through any of the following:</p>
                     <br />
-                    <p>Github: <a href="https://github.com/bcraig-13" target="_blank">https://github.com/bcraig-13</a></p>
+                    <p>Github: <a href="https://github.com/bcraig-13" rel="noreferrer" target="_blank">https://github.com/bcraig-13</a></p>
                     <br />
-                    <p>LinkedIn: <a href="https://www.linkedin.com/in/brandon-craig-4735b7203/" target="_blank">https://www.linkedin.com/in/brandon-craig-4735b7203/</a></p>
+                    <p>LinkedIn: <a href="https://www.linkedin.com/in/brandon-craig-4735b7203/" rel="noreferrer" target="_blank">https://www.linkedin.com/in/brandon-craig-4735b7203/</a></p>
                     <br />
                     <p>Email: <a href="mailto:thegamingraven13@gmail.com">thegamingraven13@gmail.com</a></p>
                 </div>

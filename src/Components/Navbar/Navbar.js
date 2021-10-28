34d70ce50899f12570d758f0c8b911543a951d4c
Brandon Craig <twilightraven13@yahoo.com>
2021-10-28 02:51:45 -0700

    Fixed hamburger color.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
       <p className="navbar-brand my-name">
         Brandon Craig
       </p>
-      <button className="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
